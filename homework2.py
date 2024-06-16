6e28ae52c5afd49b9897ab081688d2d18ab92e8b
zMOHAXz <vlastorz@gmail.com>
2024-06-16 21:07:35 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+vsego_dz=12
+vsego_chasov=1.5
+course='Python'
+odno_dz=vsego_chasov/vsego_dz
+print("Курс:",course,", всего задач:",vsego_dz,", затрачено часов:",vsego_chasov,", среднее время выполнения:",odno_dz,".")
+
